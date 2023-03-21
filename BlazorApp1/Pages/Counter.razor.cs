a6cb44fe41c976afa5a02a9cd21a48ac07ba51a3
Kaiserovich <gamer281999@gmail.com>
2023-03-21 15:53:41 +0300

    Partial class, template page, component reference

---
+++ BlazorApp1/Pages/Counter.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+
+namespace BlazorApp1.Pages
+{
+    public partial class Counter
+    {
+        [Inject] SingletonServices singleton { get; set; }
+        [Inject] TransientServices transient { get; set; }
+
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+
+            currentCount++;
+
+            singleton.Value = currentCount;
+            transient.Value = currentCount;
+        }
+    }
+}
