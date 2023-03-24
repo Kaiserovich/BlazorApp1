cdf7043004da12b093077de89239f7911373a9e6
Процукович Кирилл <gamer281999@gmail.com>
2023-03-24 10:12:47 +0300

    adding orders

--- BlazorApp1/Entities/Models/Client.cs
+++ BlazorApp1/Entities/Models/Client.cs
@@ -6,11 +6,15 @@ namespace BlazorApp1.Entities.Models
     [Table(nameof(Client))]
     public class Client
     {
+        [Required]
         public int Id { get; set; }
         [Required]
         [MaxLength(50)]    
         public string Name { get; set; }
+        [Required]
+        [Display(Name="Create Data")]
         public DateTime DataCreate { get; set; }
+        [Required]
         public ClientStatus Status { get; set; }
         public IEnumerable<Order>? Orders { get; set; }
     }
