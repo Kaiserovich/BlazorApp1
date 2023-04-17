a3209fbd106a3fe2dd14a1532ed2741a97222c0f
Процукович Кирилл <gamer281999@gmail.com>
2023-04-17 17:29:44 +0300

    Added Loggin in Client and OrderService

--- BlazorOrders/Infrastructure/ClientService.cs
+++ BlazorOrders/Infrastructure/ClientService.cs
@@ -2,6 +2,7 @@
 using BlazorOrders.Entities.Enumerations;
 using BlazorOrders.Entities.Models;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
@@ -18,29 +19,48 @@ namespace BlazorOrders.Infrastructure
         }
         public async Task<List<Client>> GetAllClientsAsync()
         {
-            // _logger.LogInfo($"Call GetAllClientsAsync{MethodBase.GetCurrentMethod().Name}");
-             _logger.LogInfo($"Call GetAllClientsAsync");
+            _logger.LogDebug($"Geting All Clients");
+
             return await _repoWrapper.Client.FindAll().ToListAsync();
         }
 
-        public async Task<List<Client>> GetClientsByStatusAsync(ClientStatus status) => await _repoWrapper.Client.FindAll().Where(x => x.Status == status).ToListAsync();
-        public async Task<Client> GetClientByIdAsync(int id) => await _repoWrapper.Client.FindByCondition(a => a.Id.Equals(id)).FirstOrDefaultAsync();
+        public async Task<List<Client>> GetClientsByStatusAsync(ClientStatus status)
+        {
+            _logger.LogDebug($"Geting Clients with status:{status}");
+
+            return await _repoWrapper.Client.FindAll().Where(x => x.Status == status).ToListAsync();
+        }
+        public async Task<Client> GetClientByIdAsync(int id)
+        {
+            _logger.LogDebug($"Geting Client with ID:{id}");
+
+            return await _repoWrapper.Client.FindByCondition(a => a.Id.Equals(id)).FirstOrDefaultAsync();
+        }
+
         public async Task CreateClientAsync(Client client)
         {
             client.DataCreate = DateTime.Now;
             client.Status = 0;
+
+            _logger.LogDebug($"Creating client:{JsonConvert.SerializeObject(client)}");
 
             _repoWrapper.Client.CreateClient(client);
             await _repoWrapper.SaveAsync();
         }
         public string UpadateClient(Client client)
         {
-            _logger.LogInfo($"Call GetAllClientsAsync");
-
             if (client.Status == ClientStatus.Inactive && _repoWrapper.Order.GetOrdersByClientId(client.Id).Where(order => order.Status == OrderStatus.New).Count() != 0)
-                return $"The client has orders in a new status. So the client's status be changed";
+            {
+                string message = $"The client (ID:{client.Id}) has orders in a new status. So the client's status hasn't changed";
+
+                _logger.LogDebug(message);
+
+                return message;
+            }
             else
             {
+                _logger.LogDebug($"Updating client:{JsonConvert.SerializeObject(client)}");
+
                 _repoWrapper.Client.Update(client);
                 _repoWrapper.SaveAsync();
 
@@ -49,6 +69,8 @@ namespace BlazorOrders.Infrastructure
         }
         public void DeleteClient(Client client)
         {
+            _logger.LogDebug($"Deleting client:{JsonConvert.SerializeObject(client)}");
+
             _repoWrapper.Client.Delete(client);
             _repoWrapper.SaveAsync();
         }
--- BlazorOrders/Infrastructure/OrderService.cs
+++ BlazorOrders/Infrastructure/OrderService.cs
@@ -1,35 +1,69 @@
 ﻿using BlazorOrders.Contracts;
 using BlazorOrders.Entities.Models;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace BlazorOrders.Infrastructure
 {
     public class OrderService : IOrderService
     {
-        private readonly IRepositoryWrapper repoWrapper;
-        public OrderService(IRepositoryWrapper repoWrapper) => this.repoWrapper = repoWrapper;
-        public async Task<List<Order>> GetAllOrdersAsync() => await repoWrapper.Order.FindAll().ToListAsync();
-        public List<Order> GetOrdersByClientId(int clientId) => repoWrapper.Order.FindByCondition(a => a.ClientId.Equals(clientId)).ToList();
-        public async Task<Order> GetOrderByIdAsync(int id) => await repoWrapper.Order.FindByCondition(a => a.Id.Equals(id)).FirstOrDefaultAsync();
-        public async Task<Order> GetLastOrdersAsync() => await repoWrapper.Order.FindAll().LastOrDefaultAsync();
+        private readonly IRepositoryWrapper _repoWrapper;
+        private readonly ILoggerManager _logger;
+        public OrderService(IRepositoryWrapper repoWrapper, ILoggerManager logger)
+        {
+            _repoWrapper = repoWrapper;
+            _logger = logger;
+        }
+        public async Task<List<Order>> GetAllOrdersAsync()
+        {
+            _logger.LogDebug($"Geting All Order");
 
+            return await _repoWrapper.Order.FindAll().ToListAsync();
+        }
+
+        public List<Order> GetOrdersByClientId(int clientId)
+        {
+            _logger.LogDebug($"Geting Orders with Client ID:{clientId}");
+
+            return _repoWrapper.Order.FindByCondition(a => a.ClientId.Equals(clientId)).ToList();
+        }
+
+        public async Task<Order> GetOrderByIdAsync(int id)
+        {
+            _logger.LogDebug($"Geting Order with ID:{id}");
+
+            return await _repoWrapper.Order.FindByCondition(a => a.Id.Equals(id)).FirstOrDefaultAsync();
+        }
+
+        public async Task<Order> GetLastOrdersAsync()
+        {
+            _logger.LogDebug($"Geting Last Order");
+
+            return await _repoWrapper.Order.FindAll().LastOrDefaultAsync();
+        }
         public async Task CreateOrderAsync(Order order)
         {
             order.Status = 0;
             order.Date = DateTime.Now;
 
-            repoWrapper.Order.Create(order);
-            await repoWrapper.SaveAsync();
+            _logger.LogDebug($"Creating order:{JsonConvert.SerializeObject(order)}");
+
+            _repoWrapper.Order.Create(order);
+            await _repoWrapper.SaveAsync();
         }
         public void UpadateOrder(Order order)
         {
-            repoWrapper.Order.Update(order);
-            repoWrapper.SaveAsync();
+            _logger.LogDebug($"Updating order:{JsonConvert.SerializeObject(order)}");
+
+            _repoWrapper.Order.Update(order);
+            _repoWrapper.SaveAsync();
         }
         public void DeleteOrder(Order order)
         {
-            repoWrapper.Order.Delete(order);
-            repoWrapper.SaveAsync();
+            _logger.LogDebug($"Deleting order:{JsonConvert.SerializeObject(order)}");
+
+            _repoWrapper.Order.Delete(order);
+            _repoWrapper.SaveAsync();
         }
     }
 }
